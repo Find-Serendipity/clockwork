60a3ab5c03021ad3bd1af2d7d9f0a88ff838887f
Find-Serendipity <find.serendipity@gmail.com>
2024-03-06 17:05:29 -0500

    Working on my pseudocode

---
+++ main.js
@@ -0,0 +1,41 @@
+// create a calculator function that
+    // takes in a defined math function and two number inputs
+        // if - getSum
+            // add two numbers
+        // if - getDividend
+            // divide two numbers
+        // if - getProduct 
+            // multiply two numbers
+        // if - getDifference
+            // subtracts two numbers
+        // if - getRemainder
+            // return the remainder if two numbers are divided
+        // if - inverseNumber
+            // return the number as a positive or negative based on input
+        // if - getSquareRoot 
+            // return the square root of a number
+        // if - convertPercentageToDecimal
+            // take in a percentage and returns the number as a decimal
+        // if - getRandomInteger 
+            // return a random integer
+        // if - getPi 
+            // return 3.14
+    // Do not show -0
+    // Return a helpful message instead of NaN if there is an error
+    // if no math function is specified
+        // return an error
+    // if number inputs are not defined, either
+        // return the specified math function with at least one number provided
+            // inverseNumber
+            // getSquareRoot
+            // convertPercentagetoDecimal
+                // This will likely require a range from 0 to 100 and convert the whole number into a decimal
+        // return the specified math function that does not require input 
+            // getPi 
+            // getRandomInteger
+        // if a different math function not specified above is input then
+                // return error, "You need numbers to do math today!"
+// create a calcResult function that
+    // takes calculator results and up to two number inputs
+        // performs identical functions to calculator, with the same restrictions
+
